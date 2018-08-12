2d6a5298518af3ec3330133c9e67f3f5483b3ddd
Aswin Karuvally <aswinbabuk@gmail.com>
2018-08-12 20:45:14 +0530

    added netdog_server.py

--- netdog_client.py
+++ netdog_client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# NetDog Server module, alpha release
+# NetDog Client module, alpha release
 # Copyright 2018, Aswin Babu Karuvally
 
 
---
+++ netdog_server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# NetDog Server module, alpha release
+# Copyright 2018, Aswin Babu Karuvally
+
+
+# the main function
+def main():
+    pass
+
+
+# call the main function
+main()
