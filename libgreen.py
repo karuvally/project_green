0e815e648c0bc7f5f7a74a1c0c9511e9b002d318
Aswin Karuvally <aswinbabuk@gmail.com>
2018-09-28 09:21:33 +0530

    minor bug fix in initialize_system

--- libgreen.py
+++ libgreen.py
@@ -499,7 +499,7 @@ def initialize_system(server = False):
 
     # if no known_server, initiate pairing
     if server == False:
-        if os.path.exists(os.path.join(config_dir, "known_server"):
+        if os.path.exists(os.path.join(config_dir, "known_server")):
             # debug: future fix, check if known_server has a valid data
             pass
         else:
