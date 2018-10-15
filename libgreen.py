e10cab4fc42fc69f89ee9d880c5d0885eb99e533
Aswin Karuvally <aswinbabuk@gmail.com>
2018-10-15 21:02:50 +0530

    bug fix in setup_network()

--- libgreen.py
+++ libgreen.py
@@ -700,23 +700,23 @@ def setup_network(server = False):
         last_known_address = known_network_info["localhost_address"]
         last_known_interface = known_network_info["interface"]
 
-    # check if network is reachable
-    if last_known_interface in interface_dump:
+        # check if network is reachable
+        if last_known_interface in interface_dump:
 
-        # get last known interface's current info
-        last_known_interface_current_info = interface_dump[last_known_interface]
-        current_address = last_known_interface_current_info["localhost_address"]
+            # get last known interface's current info
+            last_known_interface_current  = interface_dump[last_known_interface]
+            current_address = last_known_interface_current["localhost_address"]
 
-        # ping current address 
-        # if ping fails, find hosts in other interfaces
-        # if hosts found in another network, set new known_network
-        # if hosts found in current network, break
+            # ping current address 
+            # if ping fails, find hosts in other interfaces
+            # if hosts found in another network, set new known_network
+            # if hosts found in current network, break
    
-    else:
-        pass
-        # find hosts in all interfaces
-        # if hosts found in another network, set new known_network
-        # if hosts found in current network, break
+        else:
+            pass
+            # find hosts in all interfaces
+            # if hosts found in another network, set new known_network
+            # if hosts found in current network, break
 
 
     # if localhost is client, do stuff :D
