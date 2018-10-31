4b0fab3cebea805fe18872697fa134587112fdf2
Aswin Karuvally <aswinbabuk@gmail.com>
2018-10-31 20:03:34 +0530

    added main()

--- web_server.py
+++ web_server.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # NetDog Web Server, alpha release
 # Copyright 2018, Aswin Babu Karuvally
+
+# import serious stuff
+from bottle import route, run, template
+
+
+# default page
+@route("/")
+def index():
+    return("Hello")
+
+
+# the main function
+def main():
+    run(host = "0.0.0.0", port = 8080)
