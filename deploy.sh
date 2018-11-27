4fbd5864cb3dde34af0569cc8eef89677b62122a
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2018-11-27 13:47:52 +0530

    code to check if user is root

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 # deploy NetDog client
 # Copyright 2018, Aswin Babu Karuvally
+
+# exit if not root
+USERNAME=`whoami`
+if [ $USERNAME != "root" ]
+then
+    echo "run script as root!"
+    exit
+fi
+
+# create user netdog
+
+# add user netdog to group sudo
