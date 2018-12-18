388a8ee8fa652ed2cbb8957def37b66c182b8550
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2018-12-18 09:41:24 +0530

    bugfix: reset.py crashes if not config_dir

--- reset.py
+++ reset.py
@@ -6,6 +6,12 @@
 from libgreen import get_config_dir
 import shutil
 
+# get config directory path
+config_dir = get_config_dir()
+
 # do the stuff
-shutil.rmtree(get_config_dir())
+if os.path.exists(config_dir):
+    shutil.rmtree(config_dir)
+else:
+    print("configuration directory does not exist!")
 
