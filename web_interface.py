63fd1be71935816c07071e3ed3ff425c4b6ceba2
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-02-05 06:35:01 +0530

    skeleton for web interface

---
+++ web_interface.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# NetDog, web interface
+# Copyright 2018, 2019 Aswin Babu Karuvally
+
+# import serious stuff
+from libgreen import *
+from flask import Flask
+
+
+# create the webapp
+app = Flask("web_interface")
+
+
+# the homepage
+@app.route("/")
+def landing_page():
+    return "It Works!"
