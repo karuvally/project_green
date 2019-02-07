17a4af5b6567daa72f6b521d6f30f91cdb5589bc
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-02-07 21:41:03 +0530

    web_interface now returns welcome page on first run

--- web_interface.py
+++ web_interface.py
@@ -14,6 +14,14 @@ node_list = None
 # serve the landing page
 @get("/")
 def home_page():
+    # get config directory
+    config_dir = get_config_dir()
+
+    # display the welcome page on first run
+    if not os.path.exists(os.path.join(config_dir, "credentials")):
+        return static_file("welcome.html", root="static")
+
+    # else return the normal page
     return static_file("index.html", root="static")
 
 
