75f6fa495e0a76dddb5c63bc1764f1565a961cd3
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-02-12 21:08:56 +0530

    removed bottle_session import

--- web_interface.py
+++ web_interface.py
@@ -4,8 +4,7 @@
 
 # import serious stuff
 from libgreen import *
-from bottle import post, get, static_file, run, request, template, install
-import bottle_session
+from bottle import post, get, static_file, run, request, template
 
 
 # serve the landing page
