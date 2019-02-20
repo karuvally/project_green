db94308b2b43963247c48395c8c31020bb4daadd
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-02-20 13:23:01 +0530

    home_page() now returns network_chooser.html

--- web_interface.py
+++ web_interface.py
@@ -29,6 +29,8 @@ def home_page():
         return render_template("login.html")
         
     # if no known_network, return network chooser
+    if not os.path.exists(os.path.join(config_dir, "known_network")):
+        return render_template("network_chooser.html")
     
     # else, simply return the homepage
     else:
