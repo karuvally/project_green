8e8a3860b1cd9ffefac43d61797f156a67e64010
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-03-27 20:06:46 +0530

    added deploy.py

---
+++ deploy/deploy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Netdog deploy script, alpha release
+# Copyright 2019, Aswin Babu Karuvally
+
+
+# the main function
+def main():
+    pass
+    
+
+if __name__ == "__main__":
+    main()
