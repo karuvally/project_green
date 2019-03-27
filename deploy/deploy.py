bb2051a6ff880e010ed7ed0c2acc21bbeb241d17
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-03-27 20:36:34 +0530

    added parsing of arguments

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -2,10 +2,24 @@
 # Netdog deploy script, alpha release
 # Copyright 2019, Aswin Babu Karuvally
 
+# import serious stuff
+import argparse
+
 
 # the main function
 def main():
-    pass
+    # setup the argument parser
+    parser = argparse.ArgumentParser(
+        description = "Netdog deployer utility, alpha"
+    )
+    parser.add_argument(
+        "-s", 
+        "--server", 
+        help = "install the server", 
+        action = "store_true",
+        dest = "install_server"
+    )
+    arguments = parser.parse_args()
     
 
 if __name__ == "__main__":
