3f60cbcb1650df8f8073fe0441bcd318674958db
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-04-07 21:58:34 +0530

    added execute_command()

--- deploy/post_install.py
+++ deploy/post_install.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 # post installation script for Netdog
 
+# import serious stuff
+import os
+import subprocess
+
+
+# execute command
+def execute_command(command):
+    return_code = subprocess.call(command, shell=True)
+
+    if return_code != 0:
+        logging.warning(command + " cannot be executed, exiting")
+        sys.exit(1)
+
 
 # the main function
 def main():
+    # create netdog user
+    execute_command("adduser --disabled-password --gecos Netdog netdog")
+
+    # set password for netdog
     pass
 
 
