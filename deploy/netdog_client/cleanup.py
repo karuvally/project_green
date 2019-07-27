ff43d62d036184306268ff180aea7345f7d5d306
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-07-27 11:23:27 +0530

    comlpeted skeleton of main()

---
+++ deploy/netdog_client/cleanup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# cleanup leftovers when uninstalling
+
+# import the serious stuff
+import os
+import subprocess
+import sys
+
+
+# the main function
+def main():
+    # remove conky
+    # remove netdog user
+    # remove sudoer file
+    # remove share directory
+
+
+if __name__ == "__main__":
+    main()
