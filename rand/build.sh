6c4e821542c1e2c65bfac5c3ec0c4758ec821006
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-08-10 15:41:02 +0530

    bugfix: config.json was not copied

--- rand/build.sh
+++ rand/build.sh
@@ -6,9 +6,14 @@ rm /home/aswin/tmp/deploy_it -rf
 echo copying new files
 cp /home/aswin/project/deploy_it /home/aswin/tmp/ -rf
 cp /home/aswin/project/netdog/src /home/aswin/tmp/deploy_it -rf
-
-echo trying to build the package
 cd /home/aswin/tmp/deploy_it
+
+echo building netdog client 
+cp src/deploy/netdog_client/config.json ./
+./build.py
+
+echo building netdog server 
+cp src/deploy/netdog_server/config.json ./
 ./build.py
 
 echo extracting the builds from zip
